fa526aa8f55101c74e809536e052b343858a4ae7
Serg1853 <129977140+Serg1853@users.noreply.github.com>
2023-09-08 17:01:03 +0300

    end task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-const form = document.querySelector('.breed-select');
+import './styles.css';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
+const selector = document.querySelector('.breed-select');
+const divCatInfo = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+
+loader.classList.replace('loader', 'is-hidden');
+error.classList.add('is-hidden');
+divCatInfo.classList.add('is-hidden');
+
+let arrBreedsId = [];
+fetchBreeds()
+  .then(data => {
+    data.forEach(element => {
+      arrBreedsId.push({ text: element.name, value: element.id });
+    });
+    new SlimSelect({
+      select: selector,
+      data: arrBreedsId,
+    });
+  })
+  .catch(onFetchError);
+
+//  console.log('arrBreedsId', arrBreedsId);
+
+selector.addEventListener('change', onSelectBreed);
+
+function onSelectBreed(event) {
+  loader.classList.replace('is-hidden', 'loader');
+  selector.classList.add('is-hidden');
+  divCatInfo.classList.add('is-hidden');
+
+  const breedId = event.currentTarget.value;
+  fetchCatByBreed(breedId)
+    .then(data => {
+      const { url, breeds } = data[0];
+
+      divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="600"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
+      divCatInfo.classList.remove('is-hidden');
+
+      loader.classList.replace('loader', 'is-hidden');
+      selector.classList.remove('is-hidden');
+    })
+    .catch(onFetchError);
+}
+
+function onFetchError(error) {
+  selector.classList.remove('is-hidden');
+  loader.classList.replace('loader', 'is-hidden');
+
+  Notify.failure(
+    'Oops! Something went wrong! Try reloading the page or select another cat breed!',
+    {
+      position: 'center-center',
+      width: '400px',
+      fontSize: '16px',
+    }
+  );
+}
